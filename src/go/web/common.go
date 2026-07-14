ac749e7be768447d259dc8bce62eef6f10d9c6cc
PJ Franco <pgonzale@nlr.gov>
2026-07-14 16:26:33 -0600

    clear backend cache

--- src/go/web/common.go
+++ src/go/web/common.go
@@ -19,6 +19,7 @@ import (
 	"phenix/web/broker"
 	bt "phenix/web/broker/brokertypes"
 	"phenix/web/cache"
+	"phenix/web/scorch"
 	"phenix/web/util"
 	"phenix/web/weberror"
 )
@@ -45,6 +46,9 @@ func startExperiment(name string) ([]byte, error) {
 	}
 
 	defer cache.UnlockExperiment(name)
+
+	// Clear any cached SCORCH pipelines from a previous experiment run
+	scorch.DeletePipeline(name, -1, -1, false)
 
 	broker.Broadcast(
 		bt.NewRequestPolicy("experiments/start", "update", name),
