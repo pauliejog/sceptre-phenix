632e643446153956efe922b72547c3d57fdb5944
PJ Franco <pgonzale@nlr.gov>
2026-07-22 17:01:49 -0600

    fix typo

--- src/js/src/components/ScorchRuns.vue
+++ src/js/src/components/ScorchRuns.vue
@@ -139,7 +139,7 @@
           }
 
           default: {
-            if ( comp.exp !== this.exp.name ) {
+            if ( comp.exp !== this.exp ) {
               return;
             }
             let endpoint = `experiments/${comp.exp}/scorch/components/${comp.run}/${comp.loop}/${comp.stage}/${comp.name}`;
