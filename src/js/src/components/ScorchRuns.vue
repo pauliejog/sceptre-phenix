c88316e9201f83ff3f7083b821e1e42c69044f6c
PJ Franco <pgonzale@nlr.gov>
2026-07-22 16:17:48 -0600

    block old exp components

--- src/js/src/components/ScorchRuns.vue
+++ src/js/src/components/ScorchRuns.vue
@@ -139,6 +139,9 @@
           }
 
           default: {
+            if ( comp.exp !== this.exp.name ) {
+              return;
+            }
             let endpoint = `experiments/${comp.exp}/scorch/components/${comp.run}/${comp.loop}/${comp.stage}/${comp.name}`;
 
             this.$http.get(
